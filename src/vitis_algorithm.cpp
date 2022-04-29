e8bac69f0406882ac74f56545ea3bee69bd05c35
Saad Ouassil Allak <sw.allak@gmail.com>
2022-04-29 14:09:07 +0200

    feat: implement kernel compilation toggles

--- src/vitis_algorithm.cpp
+++ src/vitis_algorithm.cpp
@@ -1,5 +1,6 @@
 #include "vitis_algorithm.hpp"
 
+#include <vitis_common/common/utilities.hpp>
 #include <vitis_common/xcl2/xcl2.hpp>
 
 #include "cl_utils.hpp"
@@ -21,16 +22,28 @@ void VitisAlgorithm::LoadBinaryFile() {
     devices.resize(1);
   }
 
-  cl_int err;
+  std::string device_name = device_.getInfo<CL_DEVICE_NAME>();
+  logger_->debug(fmt::format(FMT_STRING("Found device {}"), device_name));
+
+  cl_int err(0);
   OCL_CALL(context_ = cl::Context(device_, NULL, NULL, NULL, &err), err);
   OCL_CALL(queue_ = cl::CommandQueue(context_, device_,
                                      CL_QUEUE_PROFILING_ENABLE, &err),
            err);
 
-  auto binaries = xcl::import_binary_file(GetBinaryFile());
+  logger_->debug(
+      fmt::format(FMT_STRING("Import binary file {}"), GetBinaryFile()));
+  unsigned int file_buf_size = 0;
+  // TODO(soallak): is there a mem leak of file_buf ?
+  char* file_buf = read_binary_file(GetBinaryFile(), file_buf_size);
+  cl::Program::Binaries binaries{{file_buf, file_buf_size}};
+
+  logger_->debug(
+      fmt::format(FMT_STRING("Create program for binary {}"), GetBinaryFile()));
   OCL_CALL(program_ = cl::Program(context_, devices, binaries, NULL, &err),
            err);
   is_binary_loaded_ = true;
+  logger_->debug("Finish program creating");
 }
 
 }  // namespace hwcv
